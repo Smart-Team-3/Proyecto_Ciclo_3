74c1d6d4f6be223d43610bd889775310b8b16c56
AndyGreyrat <charles.florez@udea.edu.co>
2022-08-28 15:28:01 -0500

    clases completas

--- src/main/java/com/UdeAciclo3/modelos/SystemAplication.java
+++ src/main/java/com/UdeAciclo3/modelos/SystemAplication.java
@@ -8,22 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @SpringBootApplication
-public class ModelosApplication {
-	@GetMapping("/onLine")
-	public String onLine(){
-		return "hola ciclo3";
-	}
+public class SystemAplication {
 
-	@GetMapping("/test")
-	public String test(){
-		Empresa emp= new Empresa("yopis sas","calle 10","323456","1114247");
-		emp.setNombre("yopis ltda");
-		System.out.println("se creó nombre la empresa y se renombró" );
-		return emp.getNombre();
-	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModelosApplication.class, args);
+		//SpringApplication.run(SystemAplication.class, args);
 	}
 
 }
