bdd91c16e7bbe41a0067f1bd12ed205294212b20
Yohana <leyhurta12214@gmail.com>
2022-08-27 19:27:11 -0500

    Creación Entidad Empleado

---
+++ src/main/java/com/UdeAciclo3/modelos/Entidades/Empleado.java
@@ -0,0 +1,72 @@
+package com.UdeAciclo3.modelos.Entidades;
+
+import javax.persistence.*;
+
+@Entity//entidad
+@Table(name="Empleado")//para base de datos
+public class Empleado {
+
+    @Id// autoincrementable
+    @GeneratedValue(strategy = GenerationType.AUTO)//estrategia
+    private int id;//key
+    private String nombre;
+    private String correo;
+    private String rol;
+
+    @ManyToOne//asociación empleado a empresa
+    @JoinColumn(name="empresa_id")//enlaza
+    private Empresa empresa;//llamo a Empresa debo asociar
+
+    //contructor vacio
+    public Empleado() {
+    }
+
+    //constructor sin id
+    public Empleado(String nombre, String correo, String rol, Empresa empresa) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.rol = rol;
+        this.empresa = empresa;
+    }
+
+    //get y set
+    public Empresa getEmpresa(){
+        return empresa;
+    }
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
