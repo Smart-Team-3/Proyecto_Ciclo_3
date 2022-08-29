f6a1d5d9f62153ba3e8c063b989b5a2525a4298f
Gabrielrior <gabrielricor@gmail.com>
2022-08-28 19:22:43 -0500

    Implementacion del Controller

---
+++ src/main/java/com/Proyecto_Ciclo_3/Controller/ControllerFull.java
@@ -0,0 +1,73 @@
+package com.Proyecto_Ciclo_3.Controller;
+
+
+import com.Proyecto_Ciclo_3.Modelo.Entidades.Empresa;
+import com.Proyecto_Ciclo_3.Service.EmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+public class ControllerFull {
+    @Autowired
+    EmpresaService empresaService;
+
+    @GetMapping ({"/","/VerEmpresas"})
+    public String viewEmpresas(Model model, @ModelAttribute("mensaje") String mensaje){
+        List<Empresa> listaEmpresas=empresaService.getallEmpresas();
+        model.addAttribute("emplist",listaEmpresas);
+        model.addAttribute("mensaje",mensaje);
+        return "verEmpresas"; //Llamamos al HTML
+    }
+
+    @GetMapping("/AgregarEmpresa")
+    public String nuevaEmpresa(Model model, @ModelAttribute("mensaje") String mensaje){
+        Empresa emp= new Empresa(123456);
+        model.addAttribute("emp",emp);
+        model.addAttribute("mensaje",mensaje);
+        return "agregarEmpresa";
+    }
+
+    @PostMapping("/GuardarEmpresa")
+    public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
+        if(empresaService.setorChangeEmpresaName(emp)==true){
+            redirectAttributes.addFlashAttribute("mensaje","saveOK");
+            return "redirect:/VerEmpresas";
+        }
+        redirectAttributes.addFlashAttribute("mensaje","saveError");
+        return "redirect:/AgregarEmpresa";
+    }
+
+    @GetMapping("/EditarEmpresa/{id}")
+    public String editarEmpresa(Model model, @PathVariable Integer id, @ModelAttribute("mensaje") String mensaje){
+        Empresa emp=empresaService.getEmpresaById(id);
+        //Creamos un atributo para el modelo, que se llame igualmente emp y es el que ira al html para llenar o alimentar campos
+        model.addAttribute("emp",emp);
+        model.addAttribute("mensaje", mensaje);
+        return "editarEmpresa";
+    }
+
+    @PostMapping("/ActualizarEmpresa")
+    public String updateEmpresa(@ModelAttribute("emp") Empresa emp, RedirectAttributes redirectAttributes){
+        if(empresaService.setorChangeEmpresaName(emp)){
+            redirectAttributes.addFlashAttribute("mensaje","updateOK");
+            return "redirect:/VerEmpresas";
+        }
+        redirectAttributes.addFlashAttribute("mensaje","updateError");
+        return "redirect:/EditarEmpresa";
+    }
+
+    @GetMapping("/EliminarEmpresa/{id}")
+    public String eliminarEmpresa(@PathVariable Integer id, RedirectAttributes redirectAttributes){
+        if (empresaService.EliminateEmpresa(id)==true){
+            redirectAttributes.addFlashAttribute("mensaje","deleteOK");
+            return "redirect:/VerEmpresas";
+        }
+        redirectAttributes.addFlashAttribute("mensaje", "deleteError");
+        return "redirect:/VerEmpresas";
+    }
+}
