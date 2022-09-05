492b2757115af85157be28789278e1efdbeb2294
Yohana <leyhurta12214@gmail.com>
2022-09-05 14:57:47 -0500

    Servicios Empresa

--- src/main/java/com/Proyecto_Ciclo_3/Service/EmpresaService.java
+++ src/main/java/com/Proyecto_Ciclo_3/Service/EmpresaService.java
@@ -1,1 +1,43 @@
+package com.Proyecto_Ciclo_3.Service;
 
+import com.Proyecto_Ciclo_3.Modelo.Entidades.Empresa;
+import com.Proyecto_Ciclo_3.repositorio.EmpresaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmpresaService {
+    @Autowired
+    EmpresaRepositorio empresaRepositorio;
+
+    public List<Empresa> getallEmpresas(){
+        List<Empresa> listadeEmpresas = new ArrayList<>();
+        empresaRepositorio.findAll().forEach(empresa -> listadeEmpresas.add(empresa));
+        return listadeEmpresas;
+
+    }
+    public Empresa getEmpresaById(Integer id){
+        return empresaRepositorio.findById(id).get();
+    }
+    //metodo para definir o actulizar el nombre de la empresa
+    public boolean setorChangeEmpresaName(Empresa empresa){
+        Empresa emp = empresaRepositorio.save(empresa);
+        if(empresaRepositorio.findById(emp.getId()) != null){
+            return true;
+        }
+        return false;
+    }
+    public boolean EliminateEmpresa(Integer id){
+        empresaRepositorio.deleteById(id);
+        if (getEmpresaById(id) != null){
+            return false;
+        }
+        return true;
+    }
+
+
+
+}
